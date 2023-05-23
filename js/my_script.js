74707aed79286ee53ce1333169f373cf4289ef04
Patrickhell <patrickhell19@yahoo.fr>
2023-05-23 18:33:57 +0200

    implementazione dei diversi livelli di difficoltà

--- js/my_script.js
+++ js/my_script.js
@@ -1,49 +1,59 @@
-/**onst gridElement = document.getElementById('grid');
-
-for (let index = 0; index < 64; index++) {
-    const actualCell = createElement('div', 'cell');
-
-    actualCell.addEventListener('click', function(){
-        actualCell.classList.toggle('selected');
-    });
-
-    gridElement.appendChild(createElement('div', 'cell'))
-}
-
-
-/**
- * Functions
- */
 
 /**
  * Function that creates a custom HTML element with the given tag and classes (as a string)
- *
+ *const resetButton = document.querySelector('button.reset-button');
+
+resetButton.addEventListener('click', function(){
+    distanceInputElement.value = "";
+    ageSelectElement.value = "";
+    outputElement.innerHTML = '<span class="italic-font">Generate a new ticket to see its price!</span>'
+});
  * 
  */
 
 
-    const grid = document.getElementById("grid") ;
 
-     for (let x = 1 ; x <= 100 ; x++) {
-       const cellElement = createElement ( "div" , "cell");
-       cellElement.addEventListener ( "click", function() {
-        cellElement.classList.toggle ("cell-color");
+
+
+
+
+    const createButton = document.getElementById("open-grid");
+    createButton.addEventListener ("click", function openGrid() {
+
         
-        console.log(x);
-       }
-       );
-  
-       const number = document.createElement ("cell");
-       number.append(x);
-       cellElement.appendChild (number);
-       grid.appendChild(cellElement );
-       
-    } 
-    function createElement(tagName, className){
-      const cellElement = document.createElement(tagName);
-      cellElement.className = className;
-      return cellElement;
-  }
+        const grid = document.getElementById("grid") ;
+        for (let x = 1 ; x <= 100 ; x++) {
+          const cellElement = createElement ( "div" , "cell");
+          cellElement.addEventListener ( "click", function() {
+           cellElement.classList.toggle ("cell-color");
+           
+           console.log(x);
+          }
+          );
+     
+          const number = document.createElement ("cell");
+          number.append(x);
+          cellElement.appendChild (number);
+          grid.appendChild(cellElement );
+          
+       } 
+       function createElement(tagName, className){
+         const cellElement = document.createElement(tagName);
+         cellElement.className = className;
+         return cellElement;
+     }
+      }
+    );
+   
+    const selectGrill = document.getElementById("select-button")
+    selectGrill.addEventListener ("click", function(){
+
+    });
+
+    
+
+
+    
 
 
   
